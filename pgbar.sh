763005998ef27a904eb3e94be95b9cdf99ec9115
JC-onLine <jc.gillot@gmail.com>
2021-07-31 20:15:08 +0200

    feat(lcd): Scale with mini/maxi and LCD output.

--- pgbar.sh
+++ pgbar.sh
@@ -1,24 +1,47 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
+LCD=/dev/ttyUSB0
 
-function bar() {
-	# param1: X display position.
-	# param2: Y display position.
-	# param3: bar lengt in caracters.
-	# param4: Value in %.
-	BAR_ON="="
-	BAR_OFF="-"
-	SCALE=$((($3 * $4) / 100))
-	echo "Result: " $SCALE
-	for (( i=1; i<=$3; i++ )); do
-		if [ $i -le $SCALE ]; then
-			echo -ne "$BAR_ON"
-		else
-			echo -ne "$BAR_OFF"
-		fi
-	done	
-	echo ""
+function init() {
+	echo "\xA0" > $LCD
 }
 
-#main
-bar $1 $2 $3 $4
+function gotoxy() {
+	echo "\xA1\x$1\x$2" > $LCD
+}
 
+function progressbar() {
+	X_POS=$1 # param1: X display position.
+	Y_POS=$2 # param2: Y display position.
+	B_LEN=$3 # param3: bar length in caracters.
+	VALUE=$4 # param4: Value.
+	MINI=$5  # param5: Mini value.
+	MAXI=$6  # param6: Maxi value.
+	BAR_ON="="
+	BAR_OFF="\e-"
+	
+	echo "init"
+	# Init and position display
+	init
+	gotoxy $X_POS $Y_POS
+	
+	# Scale calcul
+	RANGE=$(($MAXI - $MINI))
+	SCALE=$(( B_LEN - (($B_LEN * ($MAXI - $VALUE)) / $RANGE) ))
+	echo "Range:" $RANGE " Result: " $SCALE
+	
+	# Progressbar trace
+	for (( i=1; i<=$B_LEN; i++ )); do
+		# Active trace
+		if [ $i -le $SCALE ]; then
+			echo "$BAR_ON" > $LCD
+		# Inactive trace
+		else
+			echo "$BAR_OFF" > $LCD
+		fi
+	done	
+}
+
+# Main
+#	parameters order details:
+#	X Y Length Value MinVal MaxVal
+progressbar $1 $2 $3 $4 $5 $6
